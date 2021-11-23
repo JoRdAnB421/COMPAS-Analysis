7429f88ef7fee5f31c80a484cdaf5b417af9a7e5
Jordan Barber <barberj2@cardiff.ac.uk>
2021-11-23 15:56:21 +0000

    Removing the Postprocessing 2 file

--- Timescales.py
+++ Timescales.py
@@ -1,3 +1,11 @@
+'''
+This script will look at the timescales of different processes for
+those very hard black hole binaries. In particular the interaction
+timescale to eject a binary from a cluster, to ionise the binary and
+the merger timescale
+'''
+
+
 import pandas as pd; import numpy as np
 import matplotlib.pyplot as plt
 import os 
@@ -9,3 +17,28 @@ COMPAS_Results_path = "/COMPAS_Output_1%solar_metallicity"
 SN = pd.read_csv(cwd + COMPAS_Results_path + "/BSE_Supernovae.csv", skiprows=2)
 SP = pd.read_csv(cwd + COMPAS_Results_path + "/BSE_System_Parameters.csv", skiprows=2)
 
+invalidVals = SN.loc[(SN["SystemicSpeed "] == "          -nan")|(SN["SystemicSpeed "] == "          -nan")|(SN["SystemicSpeed "] == "          -nan")]
+if len(invalidVals)>0:
+    print("{} systems dropped".format(len(invalidVals)))
+    SN.drop(invalidVals.index, inplace=True)
+
+SN = SN.astype({"SystemicSpeed ":"float64", 
+                "ComponentSpeed(SN)":"float64", 
+                "ComponentSpeed(CP)":"float64",
+                "SemiMajorAxis ":"float64"})
+
+
+# BHBH systems that remain bound
+BHB = SN.loc[(SN["Unbound"]==0)&(SN["Stellar_Type(SN)"]==14)&(SN["Stellar_Type(CP)"]==14)]
+BHB.reset_index(drop=True, inplace=True)
+
+# BHBH systems which become unbound
+BHB_unbound = SN.loc[(SN["Unbound"]==1)&(SN["Stellar_Type(SN)"]==14)&(SN["Stellar_Type(CP)"]==14)]
+BHB_unbound.reset_index(drop=True, inplace=True)
+
+# Black holes formed in the first supernovae which break the binary
+BH1_unbound = SN.loc[(SN["Unbound"]==1)&(SN["Stellar_Type(SN)"]==14)&(~SN["    SEED    "].isin(SN_dup_1["    SEED    "]))]
+BH1_unbound.reset_index(drop=True, inplace=True)
+
+
+v_esc = np.array([50]) # Cluster escape velocity kms^-1
